8ee5865b1ea192a914b6df73c9211b9cab4affd0
Ian Spiro <ispiro@codiscope.com>
2015-10-14 17:50:32 -0400

    definitely finding this.

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -9,17 +9,8 @@ function ContributionsHandler(db) {
 
     var contributionsDAO = new ContributionsDAO(db);
 
-    this.getMyResults(req, res) 
-
-
-
-
-
-
-
-
-
-        var yr = req.params.yr ;    
+    this.getMyResults(req, res) {
+    var yr = req.params.yr ;    
     var jsonQuery = {"year":yr} ;  //add or remove comma seperated "key":values given your JSON collection
     var jsonProjection = {_id:0,"year":1,"quarter":1,"daily":1,"sms":1,"paid":1} ; //leave year out since that's specified in the query anyhow
     var jsort = {"some-thing-else":-1} ; //-1 descending or 1 ascending
