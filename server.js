822e0290ebc94e860627ae96a5cd62d40efc24d1
Ian Spiro <ispiro@codiscope.com>
2015-10-14 16:41:32 -0400

    find_test

--- server.js
+++ server.js
@@ -1,149 +1,121 @@
-"use strict";
+//Load configurations
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 
 var express = require("express");
 var favicon = require("serve-favicon");
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 var session = require("express-session");
-// var csrf = require('csurf');
 var consolidate = require("consolidate"); // Templating library adapter for Express
 var swig = require("swig");
-// var helmet = require("helmet");
+var helmet = require("helmet");
 var MongoClient = require("mongodb").MongoClient; // Driver for connecting to MongoDB
 var http = require("http");
-//var helmet = require("helmet");
-//var nosniff = require('dont-sniff-mimetype');
+
 var app = express(); // Web framework to handle routing requests
 var routes = require("./app/routes");
 var config = require("./config/config"); // Application config properties
+
 /*
-// Fix for A6-Sensitive Data Exposure
-// Load keys for establishing secure HTTPS connection
-var fs = require("fs");
-var https = require("https");
-var path = require("path");
-var httpsOptions = {
-    key: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/server.key")),
-    cert: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/server.crt"))
-};
-*/
+ // Fix for A6-Sensitive Data Exposure
+ // Load keys for establishing secure HTTPS connection
+ var fs = require("fs");
+ var https = require("https");
+ var path = require("path");
+ var httpsOptions = {
+ key: fs.readFileSync(path.resolve(__dirname, "./app/cert/key.pem")),
+ cert: fs.readFileSync(path.resolve(__dirname, "./app/cert/cert.pem"))
+ };
+ */
 
 MongoClient.connect(config.db, function(err, db) {
-    if (err) {
-        console.log("Error: DB: connect");
-        console.log(err);
 
-        process.exit(1);
-    }
-    console.log("Connected to the database: " + config.db);
+    "use strict";
 
-    /*
-    // Fix for A5 - Security MisConfig
-    // TODO: Review the rest of helmet options, like "xssFilter"
-    // Remove default x-powered-by response header
-    app.disable("x-powered-by");
-
-    // Prevent opening page in frame or iframe to protect from clickjacking
-    app.use(helmet.xframe());
-
-    // Prevents browser from caching and storing page
-    app.use(helmet.noCache());
-
-    // Allow loading resources only from white-listed domains
-    app.use(helmet.csp());
-
-    // Allow communication only on HTTPS
-    app.use(helmet.hsts());
-
-    // TODO: Add another vuln: https://github.com/helmetjs/helmet/issues/26
-    // Enable XSS filter in IE (On by default)
-    // app.use(helmet.iexss());
-    // Now it should be used in hit way, but the README alerts that could be
-    // dangerous, like specified in the issue.
-    // app.use(helmet.xssFilter({ setOnOldIE: true }));
-
-    // Forces browser to only use the Content-Type set in the response header instead of sniffing or guessing it
-    app.use(nosniff());
-    */
-
-    // Adding/ remove HTTP Headers for security
-    app.use(favicon(__dirname + "/app/assets/favicon.ico"));
+    if (err) throw err;
 
     // Express middleware to populate "req.body" so we can access POST variables
+    // parse application/json
     app.use(bodyParser.json());
+    // parse application/x-www-form-urlencoded
     app.use(bodyParser.urlencoded({
-        // Mandatory in Express v4
         extended: false
     }));
 
+    // Express middleware to populate "req.cookies" so we can access cookies
+    app.use(cookieParser());
+
     // Enable session management using express middleware
     app.use(session({
-        // TODO: Add another vuln?
-        // genid: function(req) {
-        //    return genuuid() // use UUIDs for session IDs
-        //},
-        secret: config.cookieSecret,
-        // Both mandatory in Express v4
+        resave: true,
         saveUninitialized: true,
-        resave: true
+        secret: config.cookieSecret
             /*
-            // Fix for A5 - Security MisConfig
-            // Use generic cookie name
-            key: "sessionId",
-            */
+             //Fix for A5 - Security MisConfig
+             // Use generic cookie name
+             key: "sessionId",
 
-        /*
-        // Fix for A3 - XSS
-        // TODO: Add "maxAge"
-        cookie: {
-            httpOnly: true
-            // Remember to start an HTTPS server to get this working
-            // secure: true
-        }
-        */
-
+             //Fix for A3 - XSS
+             cookie: {
+             httpOnly: true,
+             secure: true
+             }
+             */
     }));
+    /* Fix for A8 - CSRF
+     //Enable Express csrf protection
+     app.use(express.csrf());
 
-    /*
-    // Fix for A8 - CSRF
-    // Enable Express csrf protection
-    app.use(csrf());
-    // Make csrf token available in templates
-    app.use(function(req, res, next) {
-        res.locals.csrftoken = req.csrfToken();
-        next();
-    });
-    */
+     // Make csrf token available in templates
+     app.use(function(req, res, next) {
+     res.locals.csrftoken = req.csrfToken();
+     next();
+     });
+     */
 
     // Register templating engine
     app.engine(".html", consolidate.swig);
     app.set("view engine", "html");
     app.set("views", __dirname + "/app/views");
     app.use(express.static(__dirname + "/app/assets"));
+    app.use(favicon(__dirname + "/app/assets/favicon.ico"));
 
     // Application routes
     routes(app, db);
 
-    // Template system setup
-    swig.setDefaults({
+    exports.getAllResults= function(req, res) {
+    var yr = req.params.yr ;    
+    var jsonQuery = {"year":yr} ;  //add or remove comma seperated "key":values given your JSON collection
+    var jsonProjection = {_id:0,"year":1,"quarter":1,"daily":1,"sms":1,"paid":1} ; //leave year out since that's specified in the query anyhow
+    var jsort = {"some-thing-else":-1} ; //-1 descending or 1 ascending
+    db.collection("YOUR-COLLECTION_NAME", function(err, collection) {
+        collection.find( jsonQuery, jsonProjection).sort(jsort).toArray( function(err, items) {
+            res.send(items);
+        });
+    });
+        }
+
+    swig.init({
+        root: __dirname + "/app/views",
         // Autoescape disabled
         autoescape: false
             /*
-            // Fix for A3 - XSS, enable auto escaping
-            autoescape: true // default value
-            */
+             // Fix for A3 - XSS, enable auto escaping
+             autoescape: true //default value
+             */
     });
 
     // Insecure HTTP connection
     http.createServer(app).listen(config.port, function() {
         console.log("Express http server listening on port " + config.port);
     });
-
     /*
-    // Fix for A6-Sensitive Data Exposure
-    // Use secure HTTPS protocol
-    https.createServer(httpsOptions, app).listen(config.port, function() {
-        console.log("Express https server listening on port " + config.port);
-    });
-    */
+     // Fix for A6-Sensitive Data Exposure
+     // Use secure HTTPS protocol
+     https.createServer(httpsOptions, app).listen(config.port, function() {
+     console.log("Express https server listening on port " + config.port);
+     });
+     */
 
 });
